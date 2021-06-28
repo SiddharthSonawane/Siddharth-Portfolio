8cd9aab036529a8982a638622ba7494623e08bb4
Siddharth Sonawane <sidharthsonavane@gmail.com>
2021-06-28 11:22:41 +0530

    update for responsive nav

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-if (window.outerWidth >= 769) {
-  $("nav").addClass("justify-content-end navbar-right");
-  // $(".view-button").addClass(");
-  $("nav").removeClass("justify-content-center");
-}
+// if (window.outerWidth >= 769) {
+//   $("nav").addClass("justify-content-end navbar-right");
+//   $("nav").removeClass("justify-content-center");
+// }
+
+jQuery(document).ready(function ($) {
+  var alterClass = function () {
+    var ww = document.body.clientWidth;
+    if (ww < 769) {
+      $("nav").removeClass("justify-content-end navbar-right");
+      $("nav").addClass("justify-content-center");
+    } else if (ww >= 769) {
+      $("nav").addClass("justify-content-end navbar-right");
+      $("nav").removeClass("justify-content-center");
+    }
+  };
+  $(window).resize(function () {
+    alterClass();
+  });
+  //Fire it when the page first loads:
+  alterClass();
+});
